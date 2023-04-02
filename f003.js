99917d2efb171641fb30c3d2206fc4d6cf3427c8
yeahnoob <3369760+yeahnoob@users.noreply.github.com>
2023-04-02 20:43:24 +0800

    working credential mask toggle on logging level

--- f003.js
+++ f003.js
@@ -1,4 +1,17 @@
-// Function to mask the credential key in a object. That object is one of the function parameters. Logging level is one of the function parameters too.
-function maskKeyInObject(unmaskedObject, maskKeyName) {
-
+// Function to return a stringt with the credentials key masking in a object.
+// That object is one of the function parameters. Logging level is one of the function parameters too.
+// That object cannot be changed.
+// Using the replacer function in JSON.stringify() method.
+function maskKeysInObjectStringify(unmaskedObject, maskKeyNames, loggingLevel = 'INFO', maskedLength = 6) {
+    var maskedStringFromObject = JSON.stringify(unmaskedObject, function (key, value) {
+        if (loggingLevel !== 'TRACE') {
+            if (maskKeyNames.indexOf(key) > -1) {
+                return value.substring(0, maskedLength) + '*** ***' + value.substring(value.length - maskedLength, value.length);
+            }
+        }
+        return value;
+    });
+    return maskedStringFromObject;
 }
+
+module.exports = { maskKeysInObjectStringify };

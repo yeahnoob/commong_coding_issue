1d19a2b4913c4545bb88c99dd5f8a06623507b65
yeahnoob <3369760+yeahnoob@users.noreply.github.com>
2023-04-02 21:01:21 +0800

    lint with `prettier`

--- f003.js
+++ f003.js
@@ -2,15 +2,24 @@
 // That object is one of the function parameters. Logging level is one of the function parameters too.
 // That object cannot be changed.
 // Using the replacer function in JSON.stringify() method.
-function maskKeysInObjectStringify(unmaskedObject, maskKeyNames, loggingLevel = 'INFO', maskedLength = 6) {
-    return JSON.stringify(unmaskedObject, function (key, value) {
-        if (loggingLevel !== 'TRACE') {
-            if (maskKeyNames.indexOf(key) > -1) {
-                return value.substring(0, maskedLength) + '*** ***' + value.substring(value.length - maskedLength, value.length);
-            }
-        }
-        return value;
-    });
+function maskKeysInObjectStringify(
+  unmaskedObject,
+  maskKeyNames,
+  loggingLevel = "INFO",
+  maskedLength = 6
+) {
+  return JSON.stringify(unmaskedObject, function (key, value) {
+    if (loggingLevel !== "TRACE") {
+      if (maskKeyNames.indexOf(key) > -1) {
+        return (
+          value.substring(0, maskedLength) +
+          "*** ***" +
+          value.substring(value.length - maskedLength, value.length)
+        );
+      }
+    }
+    return value;
+  });
 }
 
 module.exports = { maskKeysInObjectStringify };
